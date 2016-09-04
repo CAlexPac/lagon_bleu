fe622b377168defd1c66211f2ca5d6accef95f80
Alexandru Pacuraru <pacuraru@gmail.com>
2016-09-04 22:47:45 +0300

    - Change view structure for list of fish - Add form in controller and view

--- src/AppBundle/Controller/ListOfFishController.php
+++ src/AppBundle/Controller/ListOfFishController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Fish;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class ListOfFishController extends Controller
@@ -14,7 +17,7 @@ class ListOfFishController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/list-of-fish.html.twig');
+        return $this->render('list-of-fish/list.html.twig');
     }
 
     /**
@@ -22,8 +25,33 @@ class ListOfFishController extends Controller
      */
     public function newAction(Request $request)
     {
+        $fish = new Fish();
 
+        $form = $this->createFormBuilder($fish)
+            ->add('scientific_name', TextType::class)
+            ->add('english_name', TextType::class)
+            ->add('french_name', TextType::class)
+            ->add('kreol_name', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Save fish'))
+            ->getForm();
 
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $fish = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($fish);
+            $em->flush();
+
+            return $this->redirectToRoute('list-of-fish');
+
+        }
+
+        return $this->render('list-of-fish/add.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
 }
